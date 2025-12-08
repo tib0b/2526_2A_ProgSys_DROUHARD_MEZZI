b0a040e04edf7a32979d1d0cb95f7538cb25af63
Thibaud Drouhard <thibaud.drouhard@ensea.fr>
2025-12-08 09:17:28 +0100

    q2: add option to run fortune command in shell

--- enseash.c
+++ enseash.c
@@ -3,11 +3,31 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
+#include <time.h>
+#include <wait.h>
 
 int main(void) {
-    printf("Bienvenue dans le Shell ENSEA. \n");
-    printf("Pour quitter, tapez 'exit'.\n");
-    printf("enseash %%");
-    fflush(stdout);
-    sleep(100);
+
+    char input[20];
+    printf("Welcome to ENSEA Shell. \n");
+    printf("To exit, type 'exit'.\n");
+    while(1) {
+        printf("enseash %% ");
+        fflush(stdout);
+        fgets(input, 20, stdin);
+        if (strcmp(input, "fortune\n") == 0) {
+            int ret = fork();                               // Create a child process
+            if (ret == 0) {
+                execl("/bin/fortune", "fortune", NULL);     // child executes fortune command
+                exit(0);                                    // Exit child process, if not done by execl
+            }   
+            int status;
+            wait(&status);                                  // Wait for child process to conclude 
+        }
+        else {
+            printf("Unknown command : %s", input);
+        }
+    }
 }
+
